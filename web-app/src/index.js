6f9db6dfa39cba3c4133a8d5db5aaa4b1402fd7a
Anas <Anas@Anass-MacBook-Pro.local>
2018-06-05 20:22:51 -0400

    Removed unused variable.

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import configureStore from './store/configureStore';
-
-const store = configureStore;
+import store from './store/configureStore';
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
